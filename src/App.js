dbff8ab6771beadd8e468e1035b4d71da93e6a2c
hlmnicholson <nicholson.helena@gmail.com>
2020-05-23 16:44:10 +0200

    countryPicker and line chart fix

--- src/App.js
+++ src/App.js
@@ -4,23 +4,35 @@ import { Cards, Chart, CountryPicker } from './Components';
 import { fetchData } from './api/';
 import styles from './App.module.css';
 
+import image from './images/image.png';
+
 class App extends Component {
     state = {
         data: {},
+        country: '',
     }
 
     async componentDidMount() {
         const data = await fetchData();
         this.setState({ data });
     }
+
+    handleCountryChange = async (country) => {
+        const data = await fetchData(country);
+        // fetch data
+        // set state
+        this.setState({ data: data, country: country });
+    }
+
     render() {
         const { data } = this.state;
 
         return (
             <div className={styles.container}>
-               <Cards data={data} />
-               <CountryPicker />
-               <Chart />
+                <img className={styles.image} src={image} alt="COVID-19" />
+                <Cards data={data} />
+                <CountryPicker handleCountryChange={this.handleCountryChange}/>
+                <Chart />
             </div>
         );
     }
--- src/Components/Chart/Chart.jsx
+++ src/Components/Chart/Chart.jsx
@@ -1,36 +1,50 @@
 import React, { useState, useEffect } from 'react';
-import { fetchDailyData } from '../../api';
 import { Line, Bar } from 'react-chartjs-2';
+
+import { fetchDailyData } from '../../api';
 
 import styles from './Chart.module.css';
 
 const Chart = () => {
-    const [dailyData, setDailyData] = useState({});
+    const [dailyData, setDailyData] = useState([]);
 
     useEffect(() => {
         const fetchAPI = async() => {
             setDailyData(await fetchDailyData());
         }
 
-        console.log(dailyData);
+        //console.log(dailyData);
         fetchAPI();
-    // }, [input])
-    });
+    }, [setDailyData]);
 
     const lineChart = (
-        dailyData[0]
-            ? (
+        dailyData.length ? (
                 <Line 
-                data={{
-                    labels: '',
-                    datasets: [{}, {}],
+                    data={{
+                        labels: dailyData.map(({ date }) => date),
+                        datasets: [{
+                            data: dailyData.map((data) => data.confirmed),
+                            label: 'Infected',
+                            borderColor: '#3333ff',
+                            fill: true,
+                        }, {
+                            data: dailyData.map((data) => data.deaths),
+                            label: 'Deaths',
+                            borderColor: 'red',
+                            backgroundColor: 'rgba(255, 0, 0, 0.5)',
+                            fill: true,
+                        },
+                    ],
                 }}
-        />) : null
-    )
+            />
+        ) : null
+    );
 
     return (
-        <h1>Chart</h1>
-    )
-}
+        <div className={styles.container}>
+            {lineChart}
+        </div>
+    );
+};
 
-export default Chart
+export default Chart;
